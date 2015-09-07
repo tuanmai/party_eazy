3a6518affc870a540a8080682fc80b2830fc2637
Mai Anh Tuan <perhuman@gmail.com>
2015-09-08 00:47:00 +0700

    Initial Commit

---
+++ PartyEazy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PartyEazy
+//
+//  Created by Mai Anh Tuan on 9/8/15.
+//  Copyright (c) 2015 Mai Anh Tuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
