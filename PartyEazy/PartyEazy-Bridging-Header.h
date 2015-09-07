d89df3cc8ff5e59a543c4fd48e6af13aeac770b6
Mai Anh Tuan <perhuman@gmail.com>
2015-09-08 01:34:13 +0700

    Init project with simple facebook login

---
+++ PartyEazy/PartyEazy-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  PartyEazy-Bridging-Header.h
+//  PartyEazy
+//
+//  Created by Mai Anh Tuan on 9/8/15.
+//  Copyright (c) 2015 Mai Anh Tuan. All rights reserved.
+//
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
--- PartyEazy/ViewController.swift
+++ PartyEazy/ViewController.swift
@@ -7,17 +7,43 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if ((FBSDKAccessToken.currentAccessToken()) != nil) {
+        
+        } else {
+            let loginButton : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginButton)
+            loginButton.center = self.view.center
+            loginButton.readPermissions = ["public_profile", "email", "user_friends"]
+            loginButton.delegate = self
+        }
+    }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        
+        if ((error) != nil) {
+            
+        } else if result.isCancelled {
+            
+        } else {
+            
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("User Logged Out")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 
